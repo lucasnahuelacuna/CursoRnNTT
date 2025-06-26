1c5089ca9a4d3e86963373d71ae42ef3204db4a0
lucasnahuelacuna <lucas.nahuel.acuna@gmail.com>
2025-06-25 21:29:54 -0300

    Add cards

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 function App() {
 
+  function displayMessage(message: String) {
+    console.log(message);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hello World!!</Text>
+      <Pressable style={styles.card} onPress={() => displayMessage('Counter')}>
+        <Text>Counter</Text>
+      </Pressable>
+      <Pressable style={styles.card} onPress={() => displayMessage('Todo List')}>
+        <Text>Todo List</Text>
+      </Pressable>
+      <Pressable style={styles.card} onPress={() => displayMessage('Api Request')}>
+        <Text>Api Request</Text>
+      </Pressable>
     </View>
   );
 }
@@ -12,7 +24,16 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingHorizontal: 15
   },
+  card: {
+    backgroundColor: '#ccc',
+    height: 100,
+    justifyContent: 'center',
+    marginVertical: 25,
+    alignItems: 'center',
+    borderRadius: 10
+  }
 });
 
 export default App;
