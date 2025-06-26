fc8f0e41df944eb1e91ded70aaeef4845ffe167c
lucasnahuelacuna <lucas.nahuel.acuna@gmail.com>
2025-06-25 21:03:30 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      <Text>Hello World!!</Text>
     </View>
   );
 }
